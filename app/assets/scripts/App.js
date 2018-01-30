c72a1720500822f724e6a4adf66791ac67de409e
Ovi Savescu <ovi@ovswebsites.com>
2018-01-30 10:30:04 +0200

    added faqs block

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -74,6 +74,15 @@ $('.card-details__back-btn').click(function(e){
 });
 
 
+// toggle FAQs
+
+$('.mx__faqs__answer').hide();
+$('.mx__faqs__question').click(function() {
+  var toggle = $(this).nextUntil('.mx__faqs__question');
+  toggle.slideToggle();
+  $('.mx__faqs__answer').not(toggle).slideUp();
+});
+
 // var john = new Person("John Doe", "blue");
 // john.greet();
 
